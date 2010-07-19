a2f7751b2992a7e22b01d087a89474ed71bb3771
jguibert <jguibert@29edfd95-ebd2-fbaf-e2c1-6dcbdf81cba2>
2010-07-19 10:23:27 +0000

    Add test unit, and fix many potential mistake around limit

---
+++ trunk/winstone/src/test/java/net/winstone/util/MapLoaderTest.java
@@ -0,0 +1,30 @@
+package net.winstone.util;
+
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import junit.framework.TestCase;
+import net.winstone.util.MapLoader;
+
+/**
+ * MapLoaderTest test unit for MapLoader class.
+ * 
+ * @author Jerome Guibert
+ */
+public class MapLoaderTest extends TestCase {
+    
+    public void testLoadMapTest() {
+        assertNotNull("Ressource Bundle 'maptest' must exists", ResourceBundle.getBundle("maptest"));
+        Map<String, String> result = MapLoader.load(ResourceBundle.getBundle("maptest"));
+        assertNotNull("Result of MapLoader.load must be not null", result);
+        assertEquals("Result must have a size of 3", result.size(), 3);
+        assertEquals("Key 'abs' must have value 'audio/x-mpeg'", result.get("abs"), "audio/x-mpeg");
+    }
+    
+    public void testLoadOnNullRessource() {
+        Map<String, String> result = MapLoader.load(null);
+        assertNotNull("Result of MapLoader.load must be not null", result);
+        assertEquals("Result must be empty", result.isEmpty(), true);
+    }
+    
+}
