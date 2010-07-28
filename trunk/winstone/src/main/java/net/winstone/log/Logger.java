92e3096eac652b415cdc548ae726b9b16dfe35e0
jguibert <jguibert@29edfd95-ebd2-fbaf-e2c1-6dcbdf81cba2>
2010-07-28 15:58:59 +0000

    add doc

--- trunk/winstone/src/main/java/net/winstone/log/Logger.java
+++ trunk/winstone/src/main/java/net/winstone/log/Logger.java
@@ -9,63 +9,95 @@ public interface Logger {
 
     /**
      * Log Information message.
-     * @param msg
+     * @param msg the message
      */
     public void info(final String msg);
 
     /**
      * Log Information from specified key message which will be formatted with parameters.
-     * @param key
-     * @param parameters
+     * @param key the specified key
+     * @param parameters additional parameters
      */
     public void info(final String key, final String... parameters);
 
+    /**
+     * Log Information message.
+     * @param msg the message
+     * @param t the exception (throwable) to log
+     */
     public void info(final String msg, final Throwable t);
 
+    /**
+     *  Log Warn message.
+     * @param msg the message
+     */
     public void warn(final String msg);
 
-    public void warn(String string, final String... parameters);
+    /**
+     *  Log Warn from specified key message which will be formatted with parameters.
+     * @param key the specified key
+     * @param parameters additional parameters
+     */
+    public void warn(final String key, final String... parameters);
 
+    /**
+     *  Log Warn message.
+     * @param msg the message
+     * @param t the exception (throwable) to log
+     */
     public void warn(final String msg, final Throwable t);
 
+    /**
+     *  Log Error message.
+     * @param msg the message
+     */
     public void error(final String msg);
 
+    /**
+     *  Log Error message.
+     * @param msg the message
+     * @param  t the exception (throwable) to log
+     */
     public void error(final String msg, final Throwable t);
 
     /**
      * Log Error from specified key message which will be formatted with parameters.
-     * @param t
-     * @param key
-     * @param parameters
+     * @param t the exception (throwable) to log
+     * @param key the specified key
+     * @param parameters additional parametersers
      */
     public void error(final Throwable t, final String key, final String... parameters);
 
     /**
      * Log Debug message.
-     * @param msg
+     * @param msg the message
      */
     public void debug(final String msg);
 
     /**
      * Log Debug from specified key message which will be formatted with parameters.
-     * @param key
-     * @param parameters
+     * @param key the specified key
+     * @param parameters additional parameters
      */
     public void debug(final String key, final String... parameters);
 
+    /**
+     * Log Debug message.
+     * @param msg the message
+     * @param t the exception (throwable) to log
+     */
     public void debug(final String msg, final Throwable t);
 
     /**
      * Log Trace message.
-     * @param key
-     * @param parameters
+     * @param msg  the message 
      */
     public void trace(final String msg);
 
     /**
      * Log Trace from specified key message which will be formatted with parameters.
-     * @param key
-     * @param parameters
+     * @param key the specified key
+     * @param parameters additional parameters
      */
     public void trace(final String key, final String... parameters);
 
