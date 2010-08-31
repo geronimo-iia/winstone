daab6905616b37839b688978a79d9fc0e9c06a14
jguibert <jguibert@29edfd95-ebd2-fbaf-e2c1-6dcbdf81cba2>
2010-08-31 14:55:56 +0000

    remove dependency on internal logger

--- trunk/test-logic/src/test/java/net/winstone/testCase/load/LoadTestThread.java
+++ trunk/test-logic/src/test/java/net/winstone/testCase/load/LoadTestThread.java
@@ -9,16 +9,15 @@ package net.winstone.testCase.load;
 import java.io.IOException;
 import java.io.InputStream;
 
-import net.winstone.WinstoneResourceBundle;
 
 import org.xml.sax.SAXException;
 
-import winstone.Logger;
 
 import com.meterware.httpunit.GetMethodWebRequest;
 import com.meterware.httpunit.WebConversation;
 import com.meterware.httpunit.WebRequest;
 import com.meterware.httpunit.WebResponse;
+import org.slf4j.LoggerFactory;
 
 /**
  * A single worked thread in the load testing program
@@ -27,7 +26,8 @@ import com.meterware.httpunit.WebResponse;
  * @version $Id: LoadTestThread.java,v 1.2 2006/02/28 07:32:49 rickknowles Exp $
  */
 public class LoadTestThread implements Runnable {
-    private WinstoneResourceBundle resources;
+
+    protected static org.slf4j.Logger logger = LoggerFactory.getLogger(LoadTestThread.class);
     private String url;
     private long delayBeforeStarting;
     private LoadTest loadTest;
@@ -36,10 +36,7 @@ public class LoadTestThread implements Runnable {
     private boolean interrupted;
     private LoadTestThread next;
 
-    public LoadTestThread(String url, LoadTest loadTest,
-                          WinstoneResourceBundle resources, WebConversation webConv,
-            int delayedThreads) {
-        this.resources = resources;
+    public LoadTestThread(String url, LoadTest loadTest, WebConversation webConv, int delayedThreads) {
         this.url = url;
         this.loadTest = loadTest;
         this.webConv = webConv;
@@ -50,30 +47,34 @@ public class LoadTestThread implements Runnable {
         this.thread.start();
 
         // Launch the next second's getter
-        if (delayedThreads > 0)
-            this.next = new LoadTestThread(url, loadTest, resources, webConv,
-                    delayedThreads - 1);
+        if (delayedThreads > 0) {
+            this.next = new LoadTestThread(url, loadTest, webConv, delayedThreads - 1);
+        }
     }
 
+    @Override
     public void run() {
-        if (this.delayBeforeStarting > 0)
+        if (this.delayBeforeStarting > 0) {
             try {
                 Thread.sleep(this.delayBeforeStarting);
             } catch (InterruptedException err) {
             }
+        }
 
         long startTime = System.currentTimeMillis();
 
         try {
-            if (this.webConv == null)
+            if (this.webConv == null) {
                 this.webConv = new WebConversation();
+            }
 
             // Access the URL
             WebRequest wreq = new GetMethodWebRequest(this.url);
             WebResponse wresp = this.webConv.getResponse(wreq);
             int responseCode = wresp.getResponseCode();
-            if (responseCode >= 400)
+            if (responseCode >= 400) {
                 throw new IOException("Failed with status " + responseCode);
+            }
             InputStream inContent = wresp.getInputStream();
             int contentLength = wresp.getContentLength();
             byte content[] = new byte[contentLength == -1 ? 100 * 1024
@@ -90,25 +91,26 @@ public class LoadTestThread implements Runnable {
             // Confirm the result is the same size the content-length said it
             // was
             if ((position == contentLength) || (contentLength == -1)) {
-                if (this.interrupted)
+                if (this.interrupted) {
                     return;
-                this.loadTest.incTimeTotal(System.currentTimeMillis()
-                        - startTime);
+                }
+                this.loadTest.incTimeTotal(System.currentTimeMillis() - startTime);
                 this.loadTest.incSuccessCount();
-            } else
-                throw new IOException("Only downloaded " + position + " of "
-                        + contentLength + " bytes");
+            } else {
+                throw new IOException("Only downloaded " + position + " of " + contentLength + " bytes");
+            }
         } catch (IOException err) {
-            Logger.log(Logger.DEBUG, resources, "LoadTestThread.Error", err);
+            logger.debug("Error in response", err);
         } catch (SAXException err) {
-            Logger.log(Logger.DEBUG, resources, "LoadTestThread.Error", err);
+            logger.debug("Error in response", err);
         }
     }
 
     public void destroy() {
         this.interrupted = true;
         this.thread.interrupt();
-        if (this.next != null)
+        if (this.next != null) {
             this.next.destroy();
+        }
     }
 }
