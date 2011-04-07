852b86c1b5a1da14928911981b14ab79259868ab
jguibert <jguibert@29edfd95-ebd2-fbaf-e2c1-6dcbdf81cba2>
2011-04-07 15:49:51 +0000

    some update

--- trunk/test-logic/src/main/java/net/winstone/testCase/load/Launcher.java
+++ trunk/test-logic/src/main/java/net/winstone/testCase/load/Launcher.java
@@ -4,7 +4,7 @@
  * - the common development and distribution license (CDDL), v1.0; or
  * - the GNU Lesser General Public License, v2.1 or later
  */
-package net.winstone;
+package net.winstone.testCase.load;
 
 import java.util.Map;
 import net.winstone.boot.BootStrap;
