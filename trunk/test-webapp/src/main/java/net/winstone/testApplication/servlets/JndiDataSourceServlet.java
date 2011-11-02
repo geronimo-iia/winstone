1d9beb61d89d9322fb6dc672518bd6a15659723e
jguibert <jguibert@29edfd95-ebd2-fbaf-e2c1-6dcbdf81cba2>
2011-11-02 14:27:19 +0000

    add sid

--- trunk/test-webapp/src/main/java/net/winstone/testApplication/servlets/JndiDataSourceServlet.java
+++ trunk/test-webapp/src/main/java/net/winstone/testApplication/servlets/JndiDataSourceServlet.java
@@ -1,6 +1,6 @@
 package net.winstone.testApplication.servlets;
 
-import java.io.IOException; 
+import java.io.IOException;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -12,22 +12,25 @@ import javax.sql.DataSource;
 
 /**
  * Check DataSource
+ * 
  * @author Jerome Guibert
  */
 public class JndiDataSourceServlet extends HttpServlet {
-    public static final String JNDI_DATASOURCE = "jdbc/myDatasource";
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
-            InitialContext initialContext = new InitialContext();
-            Context envCtx = (Context) initialContext.lookup("java:comp/env");
-            DataSource dataSource = (DataSource) envCtx.lookup(JNDI_DATASOURCE);
-            if (dataSource == null) {
-                throw new ServletException("Datasource 'jdbc/myDatasource' should be found");
-            }
-        } catch (NamingException ex) {
-            throw new ServletException(ex);
-        }
-    }
+	private static final long serialVersionUID = -2418327420316254139L;
+	public static final String JNDI_DATASOURCE = "jdbc/myDatasource";
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			InitialContext initialContext = new InitialContext();
+			Context envCtx = (Context) initialContext.lookup("java:comp/env");
+			DataSource dataSource = (DataSource) envCtx.lookup(JNDI_DATASOURCE);
+			if (dataSource == null) {
+				throw new ServletException("Datasource 'jdbc/myDatasource' should be found");
+			}
+		} catch (NamingException ex) {
+			throw new ServletException(ex);
+		}
+	}
 }
