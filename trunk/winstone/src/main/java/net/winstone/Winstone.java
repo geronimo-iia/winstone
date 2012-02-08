da564b0fbcb9553a615f475e75bc16c9d580d64b
jguibert <jguibert@29edfd95-ebd2-fbaf-e2c1-6dcbdf81cba2>
2012-02-08 17:02:39 +0000

    Don't forget to start the server....

--- trunk/winstone/src/main/java/net/winstone/Winstone.java
+++ trunk/winstone/src/main/java/net/winstone/Winstone.java
@@ -1,20 +1,24 @@
 package net.winstone;
 
 import net.winstone.boot.BootStrap;
+import net.winstone.boot.Server;
 
 /**
- *
- *  @author Jerome Guibert
+ * 
+ * @author Jerome Guibert
  */
 public class Winstone {
 
-    /**
-     * Main methods.
-     * @param args
-     * @throws IOException if something is wrong when reading properties files.
-     */
-    public static void main(final String[] args) {
-        BootStrap bootStrap = new BootStrap(args);
-        bootStrap.boot();
-    }
+	/**
+	 * Main methods.
+	 * 
+	 * @param args
+	 * @throws IOException
+	 *             if something is wrong when reading properties files.
+	 */
+	public static void main(final String[] args) {
+		BootStrap bootStrap = new BootStrap(args);
+		Server server = bootStrap.boot();
+		server.start();
+	}
 }
