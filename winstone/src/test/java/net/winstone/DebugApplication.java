3173ca9a917893f3eefd00493e4c2c82b22dfd1e
jguibert <jguibert@29edfd95-ebd2-fbaf-e2c1-6dcbdf81cba2>
2012-02-09 14:13:38 +0000

    Add test class for debugging

---
+++ winstone/src/test/java/net/winstone/DebugApplication.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package net.winstone;
+
+/**
+ * DebugApplication. 
+ *
+ * @author JGT
+ *
+ */
+public class DebugApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Winstone.main(new String[]{"--warfile=..\\test-webapp\\target\\test-webapp.war"} );
+		
+	}
+
+}
