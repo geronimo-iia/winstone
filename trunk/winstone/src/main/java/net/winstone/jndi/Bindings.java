ccf8642dd0d4782bb51d6705527d6ef6dc202d8a
jguibert <jguibert@29edfd95-ebd2-fbaf-e2c1-6dcbdf81cba2>
2012-02-15 15:45:37 +0000

    clean up some source and put resource under resource folder

--- trunk/winstone/src/main/java/net/winstone/jndi/Bindings.java
+++ trunk/winstone/src/main/java/net/winstone/jndi/Bindings.java
@@ -3,8 +3,8 @@ package net.winstone.jndi;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Map.Entry;
+import java.util.NoSuchElementException;
 
 import javax.naming.Binding;
 import javax.naming.CompositeName;
@@ -13,68 +13,76 @@ import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 import javax.naming.spi.NamingManager;
 
+/**
+ * 
+ * Bindings.
+ * 
+ * @author Jerome Guibert
+ * 
+ */
 public class Bindings implements NamingEnumeration<Binding> {
-    
-    private Iterator<Entry<String, Object>> iterator;
-    private Context context;
-    private Hashtable<String, Object> environnement;
-    
-    public Bindings(final Context context, final Hashtable<String, Object> environnement, final Map<String, Object> bindings) {
-        super();
-        this.context = context;
-        this.environnement = environnement;
-        this.iterator = bindings.entrySet().iterator();
-    }
-    
-    @Override
-    public void close() throws NamingException {
-        this.context = null;
-        this.environnement = null;
-        this.iterator = null;
-    }
-    
-    @Override
-    public boolean hasMore() throws NamingException {
-        if (iterator == null)
-            throw new NamingException("Enumeration has already been closed");
-        return iterator.hasNext();
-    }
-    
-    @Override
-    public Binding next() throws NamingException {
-        if (hasMore()) {
-            Entry<String, Object> entry = iterator.next();
-            
-            String name = entry.getKey();
-            Object value = entry.getValue();
-            try {
-                value = NamingManager.getObjectInstance(value, new CompositeName().add(name), this.context, this.environnement);
-            } catch (Throwable err) {
-                NamingException errNaming = new NamingException("Failed To Get Instance ");
-                errNaming.setRootCause(err);
-                throw errNaming;
-            }
-            return new Binding(name, value);
-        }
-        throw new NoSuchElementException();
-    }
-    
-    @Override
-    public boolean hasMoreElements() {
-        try {
-            return hasMore();
-        } catch (NamingException err) {
-            return false;
-        }
-    }
-    
-    @Override
-    public Binding nextElement() {
-        try {
-            return next();
-        } catch (NamingException namingException) {
-            throw new NoSuchElementException();
-        }
-    }
-    
+
+	private Iterator<Entry<String, Object>> iterator;
+	private Context context;
+	private Hashtable<String, Object> environnement;
+
+	public Bindings(final Context context, final Hashtable<String, Object> environnement, final Map<String, Object> bindings) {
+		super();
+		this.context = context;
+		this.environnement = environnement;
+		this.iterator = bindings.entrySet().iterator();
+	}
+
+	@Override
+	public void close() throws NamingException {
+		this.context = null;
+		this.environnement = null;
+		this.iterator = null;
+	}
+
+	@Override
+	public boolean hasMore() throws NamingException {
+		if (iterator == null) {
+			throw new NamingException("Enumeration has already been closed");
+		}
+		return iterator.hasNext();
+	}
+
+	@Override
+	public Binding next() throws NamingException {
+		if (hasMore()) {
+			final Entry<String, Object> entry = iterator.next();
+
+			final String name = entry.getKey();
+			Object value = entry.getValue();
+			try {
+				value = NamingManager.getObjectInstance(value, new CompositeName().add(name), this.context, this.environnement);
+			} catch (final Throwable err) {
+				final NamingException errNaming = new NamingException("Failed To Get Instance ");
+				errNaming.setRootCause(err);
+				throw errNaming;
+			}
+			return new Binding(name, value);
+		}
+		throw new NoSuchElementException();
+	}
+
+	@Override
+	public boolean hasMoreElements() {
+		try {
+			return hasMore();
+		} catch (final NamingException err) {
+			return false;
+		}
+	}
+
+	@Override
+	public Binding nextElement() {
+		try {
+			return next();
+		} catch (final NamingException namingException) {
+			throw new NoSuchElementException();
+		}
+	}
+
 }
