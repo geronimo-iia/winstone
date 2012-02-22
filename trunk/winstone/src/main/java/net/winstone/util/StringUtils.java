0fc37c8901aa97f9b861cb5618c3b15dae04d628
jguibert <jguibert@29edfd95-ebd2-fbaf-e2c1-6dcbdf81cba2>
2012-02-22 16:13:25 +0000

    Added the canonicalization of the URI path.

--- trunk/winstone/src/main/java/net/winstone/util/StringUtils.java
+++ trunk/winstone/src/main/java/net/winstone/util/StringUtils.java
@@ -1,6 +1,9 @@
 package net.winstone.util;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -185,4 +188,45 @@ public class StringUtils {
 		return buf.toString();
 	}
 
+	/**
+	 * Eliminates "." and ".." in the path. So that this method can be used for
+	 * any string that looks like an URI, this method preserves the leading and
+	 * trailing '/'.
+	 */
+	public static String canonicalPath(String path) {
+		List<String> r = new ArrayList<String>(Arrays.asList(path.split("/+")));
+		for (int i = 0; i < r.size();) {
+			String cur = (String) r.get(i);
+			if (cur.length() == 0 || cur.equals(".")) {
+				// empty token occurs for example, "".split("/+") is [""]
+				r.remove(i);
+			} else if (cur.equals("..")) {
+				// i==0 means this is a broken URI.
+				r.remove(i);
+				if (i > 0) {
+					r.remove(i - 1);
+					i--;
+				}
+			} else {
+				i++;
+			}
+		}
+
+		StringBuilder buf = new StringBuilder();
+		if (path.startsWith("/"))
+			buf.append('/');
+		boolean first = true;
+		for (Object aR : r) {
+			String token = (String) aR;
+			if (!first)
+				buf.append('/');
+			else
+				first = false;
+			buf.append(token);
+		}
+		// translation: if (path.endsWith("/") && !buf.endsWith("/"))
+		if (path.endsWith("/") && (buf.length() == 0 || buf.charAt(buf.length() - 1) != '/'))
+			buf.append('/');
+		return buf.toString();
+	}
 }
