22fdbef0f1b0498a62b511a3ed1cdbb0adb81ee2
jguibert <jguibert@29edfd95-ebd2-fbaf-e2c1-6dcbdf81cba2>
2012-12-04 16:06:38 +0000

    [FIXED JENKINS-9882] Use Java5 ExecutorService for thread management. It is a better implementation of the thread pool, except one catch about how it prefers queueing over adding more threads.

--- winstone/src/main/java/net/winstone/core/ObjectPool.java
+++ winstone/src/main/java/net/winstone/core/ObjectPool.java
@@ -11,9 +11,16 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import net.winstone.core.listener.Listener;
 import net.winstone.core.listener.RequestHandlerThread;
+import net.winstone.util.BoundedExecutorService;
 import net.winstone.util.StringUtils;
 
 import org.slf4j.LoggerFactory;
@@ -30,26 +37,18 @@ public class ObjectPool {
 	protected static org.slf4j.Logger logger = LoggerFactory.getLogger(ObjectPool.class);
 
 	/**
-	 * FLUSH PERIOD 60 second.
-	 */
-	private static final long FLUSH_PERIOD = 60000L;
-
-	/**
 	 * Default startup request handler in pool (5).
 	 */
 	private final static transient int STARTUP_REQUEST_HANDLERS_IN_POOL = 5;
 	/**
-	 * Maximum idle request handler in pool (50).
+	 * Maximum idle request handler in pool (20).
 	 */
-	private final static transient int MAX_IDLE_REQUEST_HANDLERS_IN_POOL = 50;
+	private final static transient int MAX_IDLE_REQUEST_HANDLERS_IN_POOL = 20;
 	/**
-	 * Maximum request handler in pool (1000).
+	 * Maximum request handler in pool (200).
 	 */
-	private final static transient int MAX_REQUEST_HANDLERS_IN_POOL = 1000;
-	/**
-	 * Retry period (1 second).
-	 */
-	private final static transient long RETRY_PERIOD = 1000;
+	private final static transient int MAX_REQUEST_HANDLERS_IN_POOL = 200;
+
 	/**
 	 * Start requests in pool (10).
 	 */
@@ -67,19 +66,14 @@ public class ObjectPool {
 	 */
 	private final static transient int MAX_RESPONSES_IN_POOL = 1000;
 
-	private final List<RequestHandlerThread> unusedRequestHandlerThreads;
-	private final List<RequestHandlerThread> usedRequestHandlerThreads;
-	private final List<WinstoneRequest> unusedRequestPool;
-	private final List<WinstoneResponse> unusedResponsePool;
-	private final Object requestHandlerSemaphore = Boolean.TRUE;
-	private final Object requestPoolSemaphore = Boolean.TRUE;
-	private final Object responsePoolSemaphore = Boolean.TRUE;
+	private final ExecutorService requestHandler;
+	private List<WinstoneRequest> unusedRequestPool;
+	private List<WinstoneResponse> unusedResponsePool;
+	private final Object requestPoolSemaphore = new Object();
+	private final Object responsePoolSemaphore = new Object();
 
-	private int threadIndex = 0;
 	private final boolean simulateModUniqueId;
 	private final boolean saveSessions;
-
-	private Thread thread;
 
 	int startupRequest = ObjectPool.STARTUP_REQUEST_HANDLERS_IN_POOL;
 	int maxRequestHandlesInPool = ObjectPool.MAX_REQUEST_HANDLERS_IN_POOL;
@@ -108,10 +102,20 @@ public class ObjectPool {
 		// load saveSessions
 		saveSessions = WebAppConfiguration.useSavedSessions(args);
 
-		// Build the initial pool of handler threads
-		unusedRequestHandlerThreads = new ArrayList<RequestHandlerThread>();
-		usedRequestHandlerThreads = new ArrayList<RequestHandlerThread>();
+		// idle thread will only hang around for 60 secs
+		ExecutorService es = new ThreadPoolExecutor(MAX_IDLE_REQUEST_HANDLERS_IN_POOL, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>(), new ThreadFactory() {
+			private int threadIndex;
 
+			public synchronized Thread newThread(Runnable r) {
+				String threadName = "RequestHandlerThread[" + (++threadIndex) + "]";
+				// allocate a thread to run on this object
+				Thread thread = new Thread(r, threadName);
+				thread.setDaemon(true);
+				return thread;
+			}
+		});
+
+		requestHandler = new BoundedExecutorService(es, MAX_REQUEST_HANDLERS_IN_POOL);
 		// Build the request/response pools
 		unusedRequestPool = new ArrayList<WinstoneRequest>();
 		unusedResponsePool = new ArrayList<WinstoneResponse>();
@@ -121,11 +125,6 @@ public class ObjectPool {
 		maxRequestHandlesInPool = StringUtils.intArg(args, "handlerCountMax", ObjectPool.MAX_REQUEST_HANDLERS_IN_POOL);
 		maxIdleRequestHandlesInPool = StringUtils.intArg(args, "handlerCountMaxIdle", ObjectPool.MAX_IDLE_REQUEST_HANDLERS_IN_POOL);
 
-		// Start the base set of handler threads
-		for (int n = 0; n < startupRequest; n++) {
-			unusedRequestHandlerThreads.add(new RequestHandlerThread(this, threadIndex++, simulateModUniqueId, saveSessions));
-		}
-
 		// Initialize the request/response pools
 		for (int n = 0; n < ObjectPool.START_REQUESTS_IN_POOL; n++) {
 			unusedRequestPool.add(new WinstoneRequest(ObjectPool.MAXPARAMALLOWED));
@@ -133,64 +132,13 @@ public class ObjectPool {
 		for (int n = 0; n < ObjectPool.START_RESPONSES_IN_POOL; n++) {
 			unusedResponsePool.add(new WinstoneResponse());
 		}
-
-		thread = new Thread(new Runnable() {
-			/**
-			 * Every 60s, remove unused request handler.
-			 * 
-			 * @see java.lang.Runnable#run()
-			 */
-			@Override
-			public void run() {
-				boolean interrupted = Boolean.FALSE;
-				while (!interrupted) {
-					try {
-						Thread.sleep(ObjectPool.FLUSH_PERIOD);
-						removeUnusedRequestHandlers();
-					} catch (final InterruptedException err) {
-						interrupted = Boolean.TRUE;
-					}
-				}
-				thread = null;
-			}
-		}, "WinstoneObjectPoolMgmt");
-		thread.setDaemon(Boolean.TRUE);
-		thread.start();
-	}
-
-	/**
-	 * remove Unused RequestHandlers.
-	 */
-	private void removeUnusedRequestHandlers() {
-		// Check max idle requestHandler count
-		synchronized (requestHandlerSemaphore) {
-			// If we have too many idle request handlers
-			while (unusedRequestHandlerThreads.size() > maxIdleRequestHandlesInPool) {
-				final RequestHandlerThread rh = unusedRequestHandlerThreads.get(0);
-				rh.destroy();
-				unusedRequestHandlerThreads.remove(rh);
-			}
-		}
 	}
 
 	/**
 	 * Destroy Object Pool.
 	 */
 	public void destroy() {
-		synchronized (requestHandlerSemaphore) {
-			List<RequestHandlerThread> handlerThreads = new ArrayList<RequestHandlerThread>(usedRequestHandlerThreads);
-			for (final RequestHandlerThread handlerThread : handlerThreads) {
-				releaseRequestHandler(handlerThread);
-			}
-			handlerThreads = new ArrayList<RequestHandlerThread>(unusedRequestHandlerThreads);
-			for (final RequestHandlerThread handlerThread : handlerThreads) {
-				handlerThread.destroy();
-			}
-			unusedRequestHandlerThreads.clear();
-		}
-		if (thread != null) {
-			thread.interrupt();
-		}
+		requestHandler.shutdown();
 	}
 
 	/**
@@ -199,58 +147,11 @@ public class ObjectPool {
 	 * finishes, the handler is released back into the pool.
 	 */
 	public void handleRequest(final Socket socket, final Listener listener) throws IOException, InterruptedException {
-		RequestHandlerThread rh = null;
-		synchronized (requestHandlerSemaphore) {
-			// If we have any spare, get it from the pool
-			final int unused = unusedRequestHandlerThreads.size();
-			if (unused > 0) {
-				rh = unusedRequestHandlerThreads.remove(unused - 1);
-				usedRequestHandlerThreads.add(rh);
-				ObjectPool.logger.debug("RHPool: Using pooled handler thread - used: {} unused: {}", "" + usedRequestHandlerThreads.size(), "" + unusedRequestHandlerThreads.size());
-			} // If we are out (and not over our limit), allocate a new one
-			else if (usedRequestHandlerThreads.size() < ObjectPool.MAX_REQUEST_HANDLERS_IN_POOL) {
-				rh = new RequestHandlerThread(this, threadIndex++, simulateModUniqueId, saveSessions);
-				usedRequestHandlerThreads.add(rh);
-				ObjectPool.logger.debug("RHPool: Spawning new handler thread - used: {} unused: {}]", "" + usedRequestHandlerThreads.size(), "" + unusedRequestHandlerThreads.size());
-			} // otherwise throw fail message - we've blown our limit
-			else {
-				// Possibly insert a second chance here ? Delay and one retry ?
-				// Remember to release the lock first
-				ObjectPool.logger.warn("WARNING: Request handler pool limit exceeded - waiting for retry");
-				// socket.close();
-				// throw new UnavailableException("NoHandlersAvailable");
-			}
-		}
-
-		if (rh != null) {
-			rh.commenceRequestHandling(socket, listener);
-		} else {
-			// Sleep for a set period and try again from the pool
-			Thread.sleep(ObjectPool.RETRY_PERIOD);
-			synchronized (requestHandlerSemaphore) {
-				if (usedRequestHandlerThreads.size() < ObjectPool.MAX_REQUEST_HANDLERS_IN_POOL) {
-					rh = new RequestHandlerThread(this, threadIndex++, simulateModUniqueId, saveSessions);
-					usedRequestHandlerThreads.add(rh);
-					ObjectPool.logger.debug("RHPool: Spawning new handler thread - used: {} unused: {}", "" + usedRequestHandlerThreads.size(), "" + unusedRequestHandlerThreads.size());
-				}
-			}
-			if (rh != null) {
-				rh.commenceRequestHandling(socket, listener);
-			} else {
-				ObjectPool.logger.error("Request ignored because there were no more request handlers available in the pool");
-				socket.close();
-			}
-		}
-	}
-
-	/**
-	 * Release the handler back into the pool
-	 */
-	public void releaseRequestHandler(final RequestHandlerThread rh) {
-		synchronized (requestHandlerSemaphore) {
-			usedRequestHandlerThreads.remove(rh);
-			unusedRequestHandlerThreads.add(rh);
-			ObjectPool.logger.debug("RHPool: Releasing handler thread - used: {} unused: {}", "" + usedRequestHandlerThreads.size(), "" + unusedRequestHandlerThreads.size());
+		try {
+			requestHandler.submit(new RequestHandlerThread(this.simulateModUniqueId, this.saveSessions, socket, listener));
+		} catch (RejectedExecutionException e) {
+			logger.warn("WARNING: Request handler pool limit exceeded - waiting for retry");
+			socket.close();
 		}
 	}
 
@@ -265,7 +166,7 @@ public class ObjectPool {
 			// If we have any spare, get it from the pool
 			final int unused = unusedRequestPool.size();
 			if (unused > 0) {
-				winstoneRequest = unusedRequestPool.remove(unused - 1);
+				winstoneRequest = this.unusedRequestPool.remove(unused - 1);
 				ObjectPool.logger.debug("ReqPool: Using pooled request - available: {}", "" + unusedRequestPool.size());
 			} else {
 				// If we are out, allocate a new one
@@ -304,7 +205,7 @@ public class ObjectPool {
 			// If we have any spare, get it from the pool
 			final int unused = unusedResponsePool.size();
 			if (unused > 0) {
-				rsp = unusedResponsePool.remove(unused - 1);
+				rsp = this.unusedResponsePool.remove(unused - 1);
 				ObjectPool.logger.debug("RspPool: Using pooled response - available: {}", "" + unusedResponsePool.size());
 			} // If we are out, allocate a new one
 			else {
